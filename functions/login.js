fcdd0464963ca01ecc0c5573b700148676468a99
Will Alicar <will@alicar.me>
2023-10-27 21:17:44 -0700

    implment login with serverless function

--- functions/login.js
+++ functions/login.js
@@ -1,10 +1,42 @@
-// import { InitiateAuthCommand, InitiateAuthCommandInput, InitiateAuthCommandOutput, CognitoIdentityProviderClient } from "@aws-sdk/client-cognito-identity-provider"
+import {
+  InitiateAuthCommand,
+  CognitoIdentityProviderClient,
+  AuthFlowType,
+} from "@aws-sdk/client-cognito-identity-provider";
 
-export function onRequestPost({ request, next, env }) {
-  console.log(request);
-  return new Response("nice", {
-    headers: {
-      "Set-Cookie": "coffee=good; HttpOnly; Secure",
+export async function onRequestPost({ request, next, env }) {
+  const { username, password } = await request.json();
+  console.log(env.REGION);
+  const client = new CognitoIdentityProviderClient({
+    region: env.REGION,
+  });
+  const command = new InitiateAuthCommand({
+    AuthFlow: AuthFlowType.USER_PASSWORD_AUTH,
+    AuthParameters: {
+      USERNAME: username,
+      PASSWORD: password,
     },
+    ClientId: env.COG_CLIENT_ID,
+  });
+  const res = await client.send(command);
+  if (res.AuthenticationResult) {
+    const { AccessToken, ExpiresIn, IdToken } = res.AuthenticationResult;
+    const headers = new Headers();
+    headers.append(
+      "Set-Cookie",
+      `AccessToken=${AccessToken}; HttpOnly; Secure`
+    );
+    headers.append("Set-Cookie", `IdToken=${IdToken}; HttpOnly; Secure`);
+    headers.append(
+      "Set-Cookie",
+      `ExpiresAt=${Date.now() + ExpiresIn}; HttpOnly; Secure`
+    );
+
+    return new Response("Authenticated", {
+      headers: headers
+    });
+  }
+  return new Response("Challenge Required", {
+    body: JSON.stringify(res),
   });
 }
