05cdc21613f30dc1f9999b67dfd1107ef5e3306b
Will Alicar <will@alicar.me>
2023-10-28 17:12:31 -0700

    remove the use of isExpired

--- functions/s3/_middleware.js
+++ functions/s3/_middleware.js
@@ -1,10 +1,9 @@
 // middleware to check if session is expired
 import parseCookies from "../util/parseCookies";
-import isExpired from "../util/isExpired";
 
 export async function onRequest({ request, next }) {
   const { ExpiresAt } = parseCookies(request.headers.get("cookie"));
-  if (isExpired(ExpiresAt)) {
+  if (Date.now()/1000 >= ExpiresAt) {
     return new Response("Unauthorized", { status: 401 });
   } else {
     return next();
--- functions/util/isExpired.js
+++
@@ -1,7 +0,0 @@
-export default function isExpired(timestamp) {
-    if (Date.now()/1000 >= timestamp) {
-        return true;
-    } else {
-        return false;
-    }
-}
