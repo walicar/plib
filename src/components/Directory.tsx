e0ae7c58d18abc430d37fe905c280f993361e900
Will Alicar <will@alicar.me>
2023-10-30 15:03:35 -0700

    breadcrumbs clarity

--- src/components/Directory.tsx
+++ src/components/Directory.tsx
@@ -86,9 +86,9 @@ function Directory() {
                 }}
                 className="px-3 font-semibold text-md shadow-sm dark:bg-opacity-80 bg-opacity-50 bg-gray-100 dark:bg-slate-800 rounded-md p-1 mx-2"
               >
-                {item}
+                {item == "/" ? "Home" : item.substring(0, item.length - 1)}
               </button>
-              {index != arr.length - 1 ? <span>{" > "}</span> : <></>}
+              {index != arr.length - 1 ? <span>{"/"}</span> : <></>}
             </>
           );
         })}
