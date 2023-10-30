f06b9ef4b34688d660cccb1f61ded18321ecb77c
Will Alicar <will@alicar.me>
2023-10-29 17:51:02 -0700

    change sidebar title

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -5,7 +5,7 @@ function Sidebar() {
   return (
     <>
       <span className="mx-auto sm:h-[30px] text-4xl sm:text-2xl font-semibold transition-[30px] ease-in-out delay-0 hover:text-5xl sm:hover:text-3xl">
-        <NavLink to={"/"}>my lib</NavLink>
+        <NavLink to={"/"}>plib</NavLink>
       </span>
       <ThemeButton className="h-6 w-6 sm:-mt-1 transition ease-in-out delay-0" />
     </>
